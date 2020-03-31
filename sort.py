0fc915119b1781c0bc458803baa780c076272968
JuXueDie <35058928+JuXueDie@users.noreply.github.com>
2020-03-31 15:52:05 +0800

    Add files via upload

---
+++ sort.py
@@ -0,0 +1,22 @@
+x = [4, 2, 5, 3, 1]
+
+# 排序並建立新的 List
+y = sorted(x)
+print(y)
+print(x)
+
+x = [4, 2, 5, 3, 1]
+
+# 對原本的 List 排序
+x.sort()
+print(x)
+
+# 文字排序
+com = ['Microsoft', 'Google', 'Amazon', 'Facebook', 'Apple']
+print(sorted(com))
+
+x = [4, 2, 5, 3, 1]
+
+# 反向排序
+y = sorted(x, reverse = True)
+print(y)
